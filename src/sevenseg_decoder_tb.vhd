064e3fda1b7ecb908c1d3dad8169d81eb66077d2
USAFAJosh <253311381+USAFAJosh@users.noreply.github.com>
2026-02-25 17:40:24 -0700

    Lab Complete

--- src/sevenseg_decoder_tb.vhd
+++ src/sevenseg_decoder_tb.vhd
@@ -49,23 +49,23 @@ sevenseg_decoder_uut: sevenseg_decoder
 	begin
     
     w_in <= x"0"; wait for 10 ns;
-    assert w_out = "0111111" report "error on 0" severity failure;
+    assert w_out = "1000000" report "error on 0" severity failure;
     w_in <= x"2"; wait for 10 ns;
-    assert w_out = "1011011" report "error on 2" severity failure;
+    assert w_out = "0100100" report "error on 2" severity failure;
     w_in <= x"4"; wait for 10 ns;
-    assert w_out = "1100110" report "error on 4" severity failure;
+    assert w_out = "0011001" report "error on 4" severity failure;
     w_in <= x"7"; wait for 10 ns;
-    assert w_out = "0000111" report "error on 7" severity failure;
+    assert w_out = "1111000" report "error on 7" severity failure;
     w_in <= x"8"; wait for 10 ns;
-    assert w_out = "1111111" report "error on 8" severity failure;
+    assert w_out = "0000000" report "error on 8" severity failure;
     w_in <= x"A"; wait for 10 ns;
-    assert w_out = "1110111" report "error on a" severity failure;
+    assert w_out = "0001000" report "error on a" severity failure;
     w_in <= x"C"; wait for 10 ns;
-    assert w_out = "1011000" report "error on c" severity failure;
+    assert w_out = "0100111" report "error on c" severity failure;
     w_in <= x"E"; wait for 10 ns;
-    assert w_out = "1111001" report "error on e" severity failure;
+    assert w_out = "0000110" report "error on e" severity failure;
     w_in <= x"F"; wait for 10 ns;
-    assert w_out = "1110001" report "error on f" severity failure;
+    assert w_out = "0001110" report "error on f" severity failure;
     
     wait; -- wait forever
 	end process;	
